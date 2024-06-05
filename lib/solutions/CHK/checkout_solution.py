f95bc0174a3b5b61d35c9a3877a1b7c31a01bc15
sbx_user1051 <sbx_user1051@169.254.31.233>
2024-06-05 11:56:30 +0000

    Wed Jun 05 11:56:30 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,25 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+
+    prices = {'A': 50, 'B':30, 'C':20, 'D': 15}
+    special_offers = {'A': (3, 130), 'B': (2, 45)}
+
+    if not all(item in prices for item in skus):
+        return -1
+
+    from collections import Counter
+    item_counts = Counter(skus)
+
+    sum = 0
+
+    for item, count in item_counts.items():
+        if item in special_offers:
+            offer_count, offer_price = special_offers[item]
+            sum += (count // offer_count)*offer_price
+            count %= offer_count
+
+        sum += count * prices[item]
+
+    return sum
+
