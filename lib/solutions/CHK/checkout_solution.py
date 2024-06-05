ee3a71c69a7fdeef7aaf157a4bb6bbca17c4d8df
sbx_user1051 <sbx_user1051@169.254.31.233>
2024-06-05 13:02:15 +0000

    Wed Jun 05 13:02:15 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,7 +6,7 @@ def checkout(skus):
         'G': 20, 'H': 10, 'I': 35, 'J': 60, 'K': 80, 'L': 90,
         'M': 15, 'N': 40, 'O': 10, 'P': 50, 'Q': 30, 'R': 50,
         'S': 30, 'T': 20, 'U': 40, 'V': 50, 'W': 20, 'X': 90,
-        'Y':10, 'Z': 50
+        'Y': 10, 'Z': 50
     }
 
     special_offers = {
@@ -16,7 +16,7 @@ def checkout(skus):
         'K': [(2, 150)],
         'P': [(5, 200)],
         'Q': [(3, 80)],
-        'V': [(3,130),(2,90)]
+        'V': [(3, 130), (2, 90)]
     }
 
     free_offers = {
@@ -26,7 +26,6 @@ def checkout(skus):
         'R': (3, 'Q'),
         'U': (3, 'U')
     }
-
 
     if not all(item in prices for item in skus):
         return -1
@@ -44,7 +43,6 @@ def checkout(skus):
             else:
                 item_counts[free_item] = max(0, item_counts[free_item] - free_items_count)
 
-
     for item, count in item_counts.items():
         if item in special_offers:
             for offer_count, offer_price in sorted(special_offers[item], reverse=True):
@@ -54,27 +52,23 @@ def checkout(skus):
 
     return sum
 
-
-
-
-        # in item_counts.items():
-        # if item == 'E' and item_counts[item] >= 2:
-        #     free_b_items = item_counts[item] // 2
-        #     item_counts['B'] = max(0, item_counts['B'] - free_b_items)
-        #
-        # if item in special_offers:
-        #     if isinstance(special_offers[item], list):
-        #         for offer_count, offer_price in sorted(special_offers[item], reverse=True):
-        #             sum += (count // offer_count) * offer_price
-        #             count %= offer_count
-        #
-        #     elif isinstance(special_offers[item], tuple):
-        #         offer_count, free_item = special_offers[item]
-        #         sum += (count // offer_count) * offer_count * prices[item]
-        #         count %= offer_count
-        #
-        # sum += count * prices[item]
-
+    # in item_counts.items():
+    # if item == 'E' and item_counts[item] >= 2:
+    #     free_b_items = item_counts[item] // 2
+    #     item_counts['B'] = max(0, item_counts['B'] - free_b_items)
+    #
+    # if item in special_offers:
+    #     if isinstance(special_offers[item], list):
+    #         for offer_count, offer_price in sorted(special_offers[item], reverse=True):
+    #             sum += (count // offer_count) * offer_price
+    #             count %= offer_count
+    #
+    #     elif isinstance(special_offers[item], tuple):
+    #         offer_count, free_item = special_offers[item]
+    #         sum += (count // offer_count) * offer_count * prices[item]
+    #         count %= offer_count
+    #
+    # sum += count * prices[item]
 
 
 
