5fde031fd6e5b6537e74f2ed231f13229fa3575b
sbx_user1051 <sbx_user1051@169.254.31.233>
2024-06-05 12:59:15 +0000

    Wed Jun 05 12:59:15 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,13 +1,32 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10}
+    prices = {
+        'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10,
+        'G': 20, 'H': 10, 'I': 35, 'J': 60, 'K': 80, 'L': 90,
+        'M': 15, 'N': 40, 'O': 10, 'P': 50, 'Q': 30, 'R': 50,
+        'S': 30, 'T': 20, 'U': 40, 'V': 50, 'W': 20, 'X': 90,
+        'Y':10, 'Z': 50
+    }
+
     special_offers = {
         'A': [(5, 200), (3, 130)],
         'B': [(2, 45)],
+        'H': [(10, 80), (5, 45)],
+        'K': [(2, 150)],
+        'P': [(5, 200)],
+        'Q': [(3, 80)],
+        'V': [(3,130),(2,90)]
     }
 
-    free_offers = {'E': (2, 'B'), 'F': (3, 'F')}
+    free_offers = {
+        'E': (2, 'B'),
+        'F': (3, 'F'),
+        'N': (3, 'M'),
+        'R': (3, 'Q'),
+        'U': (3, 'U')
+    }
+
 
     if not all(item in prices for item in skus):
         return -1
@@ -55,6 +74,7 @@ def checkout(skus):
         #         count %= offer_count
         #
         # sum += count * prices[item]
+
 
 
 
